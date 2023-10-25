562535eb5457f15c81e28e00329cd039ec0bda70
aymen-kacem <aymenkacem2019@gmail.com>
2023-10-25 16:12:11 +0200

    tp2

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kacem_Aymen_Tp2"
+include(":app")
